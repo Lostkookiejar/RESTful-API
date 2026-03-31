cc6fa24b7a3f1c0cdb2750d5343a4b5a7652dc01
Chong Rui Xin <rxchong1123@gmail.com>
2026-03-31 10:24:22 +0800

    likes and posts api

--- index.js
+++ index.js
@@ -17,17 +17,89 @@ const pool = new Pool({
   },
 });
 
-async function getPostgresVersion() {
+app.post("/posts", async (req, res) => {
+  const { title, content, user_id } = req.body;
   const client = await pool.connect();
   try {
-    const response = await client.query("SELECT version()");
-    console.log(response.rows[0]);
+    // Check if user exists
+    const userExists = await client.query(
+      "SELECT id FROM users WHERE id = $1",
+      [user_id],
+    );
+    if (userExists.rows.length > 0) {
+      // User exists, add post
+      const post = await client.query(
+        "INSERT INTO posts (title, content, user_id, created_at) VALUES ($1, $2, $3, CURRENT_TIMESTAMP) RETURNING *",
+        [title, content, user_id],
+      );
+      // Send new post data back to client
+      res.json(post.rows[0]);
+    } else {
+      // User does not exist
+      res.status(400).json({ error: "User does not exist" });
+    }
+  } catch (err) {
+    console.log(err.stack);
+    res
+      .status(500)
+      .json({ error: "Something went wrong, please try again later!" });
   } finally {
     client.release();
   }
-}
+});
 
-getPostgresVersion();
+app.post("/likes", async (req, res) => {
+  const { user_id, post_id } = req.body;
+
+  const client = await pool.connect();
+
+  try {
+    const newLike = await client.query(
+      "INSERT INTO likes (user_id, post_id, created_at) VALUES ($1, $2, CURRENT_TIMESTAMP) RETURNING *",
+      [user_id, post_id],
+    );
+
+    res.json(newLike.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("An error occurred, please try again.");
+  } finally {
+    client.release();
+  }
+});
+
+app.delete("/likes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const client = await pool.connect();
+
+  try {
+    await client.query("DELETE FROM likes WHERE id = $1", [id]);
+    res.json({ message: "Like Deleted Successfully" });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("An error occurred, please try again");
+  } finally {
+    client.release();
+  }
+});
+
+app.get("/likes/post/:post_id", async (req, res) => {
+  const { post_id } = req.params;
+  const client = await pool.connect();
+  try {
+    const likes = await client.query(
+      "SELECT users.username from likes inner join users on likes.user_id = users.id where likes.post_id = $1",
+      [post_id],
+    );
+    res.json(likes.rows);
+  } catch (err) {
+    console.error(err.stack);
+    res.status(500).send("An error occurred, please try again");
+  } finally {
+    client.release();
+  }
+});
 
 app.post("/signup", async (req, res) => {
   const client = await pool.connect();
